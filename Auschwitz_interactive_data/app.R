0a7e84ec99a8e24a74c887d3268e31b3a5f4a814
AbbassSleiman <abbass.sleiman2003@gmail.com>
2024-03-31 16:22:04 -0400

    Finalized data cleaning and shiny
    Amended the cleaned data for use in the shiny app. Completed the shiny interactive table and graph.

--- Auschwitz_interactive_data/app.R
+++ Auschwitz_interactive_data/app.R
@@ -2,46 +2,89 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(here)
+library(readr)
+library(DT)
 
-data <- read_csv(here("data/raw_data/Auschwitz_data.csv"))
+Auschwitz_data <- read_csv(here("data/analysis_data/cleaned_Auschwitz_data.csv"))
 
-# Define UI for application that draws a histogram
+# UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Auschwitz Victims"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      # Input for selecting groups
+      selectInput("categories", "Select Category:",
+                  choices = c("Religion", "Birthplace", "Residence"),
+                  selected = "Religion"),
+      uiOutput("category_checkbox")
+    ),
+    
+    mainPanel(
+      plotOutput("graph"),
+      DTOutput("table")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+# Server
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  # Render checkbox for Religion
+  output$category_checkbox <- renderUI({
+    if (input$categories == "Religion") {
+      checkboxGroupInput("ChosenReligions",
+                         "Select Religions:",
+                         choices = unique(Auschwitz_data$religion))
+    }
+  })
+  
+  # Render graph
+  output$graph <- renderPlot({
+    if (input$categories == "Religion") {
+      specified_religions <- input$ChosenReligions
+      categorized_religion_data <- Auschwitz_data |>
+        filter(religion %in% specified_religions) |>
+        group_by(religion) |>
+        summarize(Count = n())
+      
+      ggplot(categorized_religion_data, aes(x = religion, y = Count, fill = religion)) +
+        geom_bar(stat = "identity") +
+        theme_classic() +
+        labs(x = "Religion", y = "Number of Victims", title = "Auschwitz Victims by Religion") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    }
+    
+  })
+  
+  output$table <- renderDT({
+    # Initialize data variable
+    data <- NULL
+    
+    # Filter data based on selected category
+    if (input$categories == "Religion") {
+      # Count the number of victims for each religion
+      data <- Auschwitz_data %>%
+        group_by(religion) %>%
+        summarize(Count = n())
+    } else if (input$categories == "Birthplace") {
+      # Count the number of victims for each birthplace
+      data <- Auschwitz_data %>%
+        group_by(birthplace) %>%
+        summarize(Count = n())
+    } else if (input$categories == "Residence") {
+      # Count the number of victims for each residence
+      data <- Auschwitz_data %>%
+        group_by(residence) %>%
+        summarize(Count = n())
+    }
+    
+    # Render table
+    datatable(data)
+  })
 }
 
-# Run the application 
+
+
+# Run the application
 shinyApp(ui = ui, server = server)
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,24 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the Auschwitz raw data for use in the shiny app
+# Author: Abbass Sleiman
+# Date: 31 March 2024
+# Contact: abbass.sleiman@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
 
 #### Workspace setup ####
 library(tidyverse)
+library(here)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv(here("data/raw_data/Auschwitz_data.csv"))
 
-cleaned_data <-
-  raw_data |>
+cleaned_data <- raw_data |>
   janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  mutate(date_of_birth = as.Date(date_of_birth)) |>
+  mutate(date_of_death = as.Date(date_of_death)) |>
+  na.omit()
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+#### Save Data ####
+write_csv(cleaned_data, "data/analysis_data/cleaned_Auschwitz_data.csv")
+
